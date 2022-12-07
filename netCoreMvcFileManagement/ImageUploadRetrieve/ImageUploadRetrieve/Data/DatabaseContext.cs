c8937f56b80fbc24f09a01b432880885b4e5dcce
bnfreedeveloper <bn.freedeveloper@gmail.com>
2022-12-07 13:48:53 +0100

    imagesManagement

---
+++ netCoreMvcFileManagement/ImageUploadRetrieve/ImageUploadRetrieve/Data/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using ImageUploadRetrieve.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageUploadRetrieve.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        DbSet<ImageModel> Images { get; set; }
+    }
+}
---
+++ netCoreMvcFileManagement/ImageUploadRetrieve/ImageUploadRetrieve/Models/ImageModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImageUploadRetrieve.Models
+{
+    public class ImageModel
+    {
+       [Key]
+       public int ImageId { get; set; }
+       [Column(TypeName ="nvarchar(50)")]
+       [Required]
+       public string? Title { get; set; }
+       [Column(TypeName = "nvarchar(100)")]
+       [Required]
+       public string? ImageName { get; set; }    
+    }
+}
